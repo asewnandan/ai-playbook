bdff2c3d266a62d66c9b74afd650a85df970e0b9
Ash Sewnandan <9801346+asewnandan@users.noreply.github.com>
2026-06-13 20:22:14 +0200

    chore: configured Lucode Starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import lucode from 'lucode-starlight';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			plugins: [
+				lucode({
+					navLinks: [
+						{ label: 'Docs', link: '/guides/getting-started/' },
+						{ label: 'API', link: '/reference/plugin-api/' },
+					],
+				}),
+			],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
